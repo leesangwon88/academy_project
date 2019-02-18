f2a422741f9aaee4ea74877976061c9f6c4dd32c
junhe <junhe@DESKTOP-LDQPMCC>
2019-02-18 12:19:52 +0900

    # WARNING: head commit changed in the meantime
    df

--- src/main/java/com/shopping/mall/member/memberinfo.java
+++ src/main/java/com/shopping/mall/member/memberinfo.java
@@ -1,5 +1,59 @@
 package com.shopping.mall.member;
 
-public class memberinfo {
+import java.math.BigDecimal;
 
+public class memberinfo {
+	private String member_mail;
+	private String member_id;
+	private String member_pw;
+	private String member_name;
+	private BigDecimal member_phone;
+	private String member_birth;
+	private String member_gender;
+	public memberinfo() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getMember_mail() {
+		return member_mail;
+	}
+	public void setMember_mail(String member_mail) {
+		this.member_mail = member_mail;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pw() {
+		return member_pw;
+	}
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public BigDecimal getMember_phone() {
+		return member_phone;
+	}
+	public void setMember_phone(BigDecimal member_phone) {
+		this.member_phone = member_phone;
+	}
+	public String getMember_birth() {
+		return member_birth;
+	}
+	public void setMember_birth(String member_birth) {
+		this.member_birth = member_birth;
+	}
+	public String getMember_gender() {
+		return member_gender;
+	}
+	public void setMember_gender(String member_gender) {
+		this.member_gender = member_gender;
+	}
+	
 }
