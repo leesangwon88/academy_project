e1fda0eeab77023ad52e4da7dfad0e54b6976c2d
junhe <junhe@DESKTOP-LDQPMCC>
2019-02-20 12:57:28 +0900

    adf

--- src/main/java/com/shopping/mall/HomeController.java
+++ src/main/java/com/shopping/mall/HomeController.java
@@ -12,52 +12,38 @@ import com.shopping.mall.member.memberinfo;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private memberDAO md;
-	
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
 		req.setAttribute("headerPage", "common/header.jsp");
 		req.setAttribute("footer", "common/footer.jsp");
-		return "index";	
-	}	
+		return "index";
+	}
+
 	@RequestMapping(value = "/login.go", method = RequestMethod.GET)
 	public String login(HttpServletRequest req) {
 		req.setAttribute("headerPage", "../common/header.jsp");
 		req.setAttribute("footer", "../common/footer.jsp");
-		return "member/login";	
+		return "member/login";
 	}
+
 	@RequestMapping(value = "/newMember.go", method = RequestMethod.GET)
 	public String newMember(HttpServletRequest req) {
 		req.setAttribute("headerPage", "../common/header.jsp");
 		req.setAttribute("footer", "../common/footer.jsp");
 		return "member/member_join";
 	}
+
 	@RequestMapping(value = "/logInCheck.go", method = RequestMethod.POST)
-	public String logInCheck(HttpServletRequest req,memberinfo mi) {
-		if (md.logInCheck(mi, req)) {
-			req.setAttribute("headerPage", "../common/header.jsp");
-			req.setAttribute("footer", "../common/footer.jsp");
-			return "index";
-		}else{
-			req.setAttribute("headerPage", "../common/header.jsp");
-			req.setAttribute("footer", "../common/footer.jsp");
-			return "member/login";
-		}
+	public String logInCheck(HttpServletRequest req) {
+		return "member/login";
 	}
-	@RequestMapping(value = "/newMemberCheck.go", method = RequestMethod.POST)
-	public String newMemberCheck(HttpServletRequest req,memberinfo mi) {
-		if (md.newMemberCheck(mi, req)) {
-			req.setAttribute("headerPage", "../common/header.jsp");
-			req.setAttribute("footer", "../common/footer.jsp");
-			return "index";
-		}else{
-			req.setAttribute("headerPage", "../common/header.jsp");
-			req.setAttribute("footer", "../common/footer.jsp");
-			return "member/member_join";
-		}
+
+	@RequestMapping(value = "/newMemberCheck", method = {RequestMethod.GET,RequestMethod.POST})
+	public String newMemberCheck(HttpServletRequest req) {
+		req.setAttribute("headerPage", "../common/header.jsp");
+		req.setAttribute("footer", "../common/footer.jsp");
+		return "index";
 	}
-	
+
 }
